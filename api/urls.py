81b0e684d3c587dfd04faa5efd7d8cd4727171d8
mubaarock <mustophamubarack17@gmail.com>
2024-11-19 03:33:19 +0100

    Use modelviewsets

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
 from . import views
 
-urlpatterns = [
-    path("products/", views.ApiProducts.as_view()),
-    path("products/<uuid:pk>", views.ApiProduct.as_view()),
-    path("categorys", views.ApiCategorys.as_view()),
-    path("categorys/<uuid:pk>", views.ApiCategory.as_view()),
-]
+router = DefaultRouter()
+router.register("products", views.ProductViewSet)
+router.register("categorys", views.CategoryViewSet)
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path("products/", views.ProductsView.as_view()),
+#     path("products/<uuid:pk>", views.ProductView.as_view()),
+#     path("categorys", views.CategorysView.as_view()),
+#     path("categorys/<uuid:pk>", views.CategoryView.as_view()),
+# ]
--- api/views.py
+++ api/views.py
@@ -3,11 +3,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import (
-    ListCreateAPIView,
-    RetrieveUpdateDestroyAPIView,
-    GenericAPIView,
-)
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from .serializers import ProductSerializer, CategorySerializer
 from storeapp.models import Product, Category
@@ -15,22 +12,11 @@ from storeapp.models import Product, Category
 
 # Create your views here.
 # CBV'S
-class ApiProducts(ListCreateAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class ApiProduct(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-
-class ApiCategorys(ListCreateAPIView):
+class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
-
-class ApiCategory(RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
